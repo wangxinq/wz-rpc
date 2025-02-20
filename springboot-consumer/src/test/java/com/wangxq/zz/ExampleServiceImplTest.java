c033da2c4679afa41229024bbe762085ff6b6386
wangxinq <2154659447@qq.com>
2025-02-20 16:34:56 +0800

    启动机制与注解驱动

---
+++ springboot-consumer/src/test/java/com/wangxq/zz/ExampleServiceImplTest.java
@@ -0,0 +1,17 @@
+import com.wangxq.zz.ExampleServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+class ExampleServiceImplTest {
+
+    @Resource
+    private ExampleServiceImpl exampleService;
+
+    @Test
+    void test1() {
+        exampleService.test();
+    }
+}
